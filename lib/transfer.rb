b2ff3238b56030373ce76607a97194ccb64b6f0b
Mark May <mamark90@gmail.com>
2019-06-23 10:44:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :transfer, :bankaccount, :sender,
+                :receiver, :amount, :status
+  attr_reader
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    true if sender.valid? == receiver.valid?
+  end
+
+  def execute_transaction
+    binding.pry
+  end
+
 end
